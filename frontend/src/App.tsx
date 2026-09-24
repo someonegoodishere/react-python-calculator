1ede444b93cb6a6b27418fc26c76f82057488d8a
someonegoodishere <someonegood@atomicmail.io>
2026-09-24 10:12:44 -0400

    Add App component and styles

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import Calculator from "./components/Calculator"
+import "./App.css"
+
+export default function App() {
+  return (
+    <div className="app">
+      <Calculator />
+    </div>
+  )
+}
